3b9efc7ad26ea21ead5e69612058883b845de626
Nicolas Rey <nicorv@macondo-VB.(none)>
2013-02-03 21:33:52 -0600

    Project 6 start

---
+++ eu0006/eu0006.h
@@ -0,0 +1,18 @@
+#ifndef eu0006_h
+#define eu0006_h
+
+#include"principal.h"
+
+#include"euler.h"
+
+class eu0006 : public euler{
+
+public:
+  eu0006(){};
+  ~eu0006(){};
+  void solucion();
+  void printsolution();
+
+};
+
+#endif
